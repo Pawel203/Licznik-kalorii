4724a84f122baadf8dc7e9035ad9f6289f092c1a
Pawel203 <pawelowczarzak16@gmail.com>
2026-03-30 09:25:45 +0200

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Licznik_kalorii
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Śniadanie_Dodaj(object sender, RoutedEventArgs e)
+        {
+            Śniadanie.Visibility = Visibility.Collapsed;
+
+            MainFrame.Visibility = Visibility.Visible;
+
+            MainFrame.Navigate(new Posiłki());
+        }
+
+        private void Obiad_Dodaj(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Kolacja_Dodaj(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Posiłki.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Licznik_kalorii
+{
+    public partial class Posiłki : Page
+    {
+        int B = 0;
+        int T = 0;
+        int W = 0;
+        int Kalorie = 0;
+        public Posiłki()
+        {
+            InitializeComponent();
+        }
+        private void Cofnij_Click(object sender, RoutedEventArgs e)
+        {
+            var mainWindow = (MainWindow)Application.Current.MainWindow;
+            mainWindow.Śniadanie.Visibility = Visibility.Visible;
+            mainWindow.MainFrame.Visibility = Visibility.Collapsed;
+
+        }
+
+        private void DodajOwsianka(object sender, RoutedEventArgs e)
+        {
+
+
+
+            var mainWindow = (MainWindow)Application.Current.MainWindow;
+            mainWindow.Śniadanie.Visibility = Visibility.Visible;
+            mainWindow.MainFrame.Visibility = Visibility.Collapsed;
+        }
+
+        private void DodajJajecznica(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
+
